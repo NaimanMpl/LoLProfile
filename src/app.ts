e2ad9b3ef42e57fd933cdf9457a320abc319ad88
= <=>
2022-06-10 15:25:10 +0200

    Added Webpack

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,21 @@
-import { readFileSync } from "fs";
-import { join } from 'path';
-import { PlatformId, RiotAPI } from '@fightmegg/riot-api';
+const fs = require('fs');
+const path = require('path');
+const riotapi = require('@fightmegg/riot-api');
+
 
 function syncReadFile(filename: string): string {
-    const result = readFileSync(join(__dirname, filename), 'utf-8');
+    const result = fs.readFileSync(path.join(__dirname, filename), 'utf-8');
     return result;
 }
 
-const API_KEY = syncReadFile('../src/key.txt');
+const API_KEY: string = syncReadFile('../src/key.txt');
 
 (async () => {
-    const rAPI = new RiotAPI(API_KEY);
+    const rAPI = new riotapi.RiotAPI(API_KEY);
     const summoner = await rAPI.summoner.getBySummonerName({
-        region: PlatformId.EUW1,
+        region: riotapi.PlatformId.EUW1,
         summonerName: 'ZelphiiX'
     });
     console.log(summoner);
 });
+console.log('There');
