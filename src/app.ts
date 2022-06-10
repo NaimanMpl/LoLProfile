8da927a241dfcc1200f3de40a28e21c867f30104
= <=>
2022-06-10 04:02:01 +0200

    Update Match History Card

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
-console.log('Hello world!');
+import { readFileSync } from "fs";
+import { join } from 'path';
+import { PlatformId, RiotAPI } from '@fightmegg/riot-api';
+
+function syncReadFile(filename: string): string {
+    const result = readFileSync(join(__dirname, filename), 'utf-8');
+    return result;
+}
+
+const API_KEY = syncReadFile('../src/key.txt');
+
+(async () => {
+    const rAPI = new RiotAPI(API_KEY);
+    const summoner = await rAPI.summoner.getBySummonerName({
+        region: PlatformId.EUW1,
+        summonerName: 'ZelphiiX'
+    });
+    console.log(summoner);
+});
