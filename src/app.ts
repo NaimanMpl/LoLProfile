061197b2fbe39a462e283bb8e02a86151e9798f8
Naïman <naimanmpl@gmail.com>
2022-06-11 22:48:55 +0200

    Added Browserify

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,11 @@
-import { readFileSync } from 'fs';
-import { join } from 'path';
 import { RiotAPI, PlatformId } from '@fightmegg/riot-api';
 
-
-function syncReadFile(filename: string): string {
-    const result = readFileSync(join(__dirname, filename), 'utf-8');
-    return result;
-}
-
-const API_KEY: string = syncReadFile('../api_key.txt');
-
+// const API_KEY: string = readFileSync(join(__dirname, '../key.txt'), 'utf-8');
+const API_KEY = 'RGAPI-68fb2b40-6509-46b2-b99f-41d9e538a62b';
 const rAPI = new RiotAPI(API_KEY);
+
 rAPI.summoner.getBySummonerName({
     region: PlatformId.EUW1,
     summonerName: 'ZelphiiX'
-}).then(summoner => {console.log(summoner)});
+}).then(() => { console.log('Hi!') }).catch(() => { console.log('There!'); });
+console.log('Hello World!');
